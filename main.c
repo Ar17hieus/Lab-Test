d3b2430a796e51e930de69a2fb83a00a0d19ba7a
Azreef <zhoreefm@gmail.com>
2022-12-25 23:00:44 +0800

    Added Features
    - GUI Panels
    - Edit Colors

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#define RAYGUI_IMPLEMENTATION
 #include "raylib.h"
 #include <stdio.h>
 #include <string.h>
+#include "raygui.h"
 
 struct objectData
 {
@@ -12,6 +14,7 @@ struct objectData
     float scale;
     bool isSelected;
 };
+
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -27,18 +30,6 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Lab Test 2");
 
 
-    //Declare Object
-    struct objectData object[3][10];
-
-    //total Objects
-    int totalTable = 3;
-    
-    /** ======================================================================
-    object[x][y]
-    // x = object type (chair,table,etc2)
-    // y = object id (chair1,chair2,chair3, etc2)
-    ======================================================================**/
-
 
     // Define the camera to look into our 3d world
     Camera camera = { 0 };
@@ -48,11 +39,40 @@ int main(void)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-    
+
+    //Misc Variable
     int selectedType = 0;
     int selectedId = 0;
-    bool currentlySelecting;
+    bool currentlySelecting = false;
+    bool isGUIOpen = false;
 
+    /** ======================================================================
+    INFO
+    HOW TO ADD OBJECT
+    1)Add variable to set max object
+    2)Load Models
+    3)Load Texture
+    4)Set Model's Default Position
+    5)Set Model's Size (Used to create bounding box)
+    6)Set Model's default value (for colors and isSelected)
+  
+    ARRAY GUIDE
+    object[x][y];
+    // x = object type (chair,table,etc2)
+    // y = object id (chair1,chair2,chair3, etc2)
+
+    Current Models
+    [0] - Tables
+    ======================================================================**/
+
+    //Declare Object
+    struct objectData object[3][10];
+
+    //total Objects
+    int totalType = 1;
+    int totalTable = 3;
+
+    
     //Load Models
     //Tables
     Model tempModel;
@@ -68,14 +88,15 @@ int main(void)
     //Loads Texture
     Texture2D tempTexture = LoadTexture("resources/models/obj/castle_diffuse.png"); // Load model texture 1 
 
-
     tempTexture = LoadTexture("resources/table/tex/WoodSeemles1.png");
     object[0][0].model.materials[0].maps[MATERIAL_MAP_OCCLUSION].texture = tempTexture; 
+    
     
     // Set model position
     object[0][0].position = (Vector3){-10,6,30};
     object[0][1].position = (Vector3){-10,6,0};
     object[0][2].position = (Vector3){-10,6,-30};
+
 
     // Set model Size (for bounding box)
     for (int i = 0; i < totalTable; i++)
@@ -84,11 +105,15 @@ int main(void)
     }
     
 
-    // NOTE: bounds are calculated from the original size of the model,
-    // if model is scaled on drawing, bounds must be also scaled
+    // set Object's default values
+    for (int i = 0; i < totalTable; i++)
+    {
+        object[0][i].color= WHITE;
+        object[0][i].isSelected= false;
+    }
 
+  
     SetCameraMode(camera, CAMERA_FREE);     // Set a free camera mode
-
 
     
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -113,22 +138,36 @@ int main(void)
         //Select Object
         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-            currentlySelecting = false;
-            bool foundModel = false; //to avoid higlighting multiple objects
-            for(int i = 0; i < totalTable; i++)
+            //Make sure that mouse is not using GUI
+            bool mouseInGUI = false;
+            if(GetMousePosition().x >(GetScreenWidth() -  GetScreenWidth()/4)) //Mouse is in GUI Panel
             {
-                if(GetRayCollisionBox(GetMouseRay(GetMousePosition(), camera), object[0][i].bounds).hit && !foundModel)
+                mouseInGUI = true;
+            }
+
+            if(!(mouseInGUI && isGUIOpen))
+            {
+                bool foundModel = false; //to avoid higlighting multiple objects
+                isGUIOpen = false;
+                currentlySelecting = false;
+                for(int i = 0; i < totalTable; i++)
                 {
-                    object[0][i].isSelected = true;
-                    selectedType = 0;
-                    selectedId = i;
-                    currentlySelecting = true;
-                    foundModel = true;
+                    if(GetRayCollisionBox(GetMouseRay(GetMousePosition(), camera), object[0][i].bounds).hit && !foundModel)
+                    {
+                        object[0][i].isSelected = true;
+                        selectedType = 0;
+                        selectedId = i;
+                        currentlySelecting = true;
+                        foundModel = true;
+                        isGUIOpen = true;
+                    }
+                    else
+                    {
+                        object[0][i].isSelected = false; 
+                    }
+
                 }
-                else
-                {
-                    object[0][i].isSelected = false;    
-                }
+           
             }
         }
         
@@ -138,7 +177,6 @@ int main(void)
         {
             if (IsKeyDown(KEY_W))
             {
-                
                 object[selectedType][selectedId].position.z++;
             }
             else if(IsKeyDown(KEY_S))
@@ -180,10 +218,9 @@ int main(void)
 
 
                 //Draw Models
-
                 for(int i = 0; i < totalTable; i++)
                 {
-                    DrawModel(object[0][i].model,object[0][i].position,object[0][i].scale,WHITE);
+                    DrawModel(object[0][i].model,object[0][i].position,object[0][i].scale,object[0][i].color);
                     DrawModelWires(object[0][i].model,object[0][i].position,object[0][i].scale,BLACK);
                     
                     if(object[0][i].isSelected)
@@ -192,12 +229,28 @@ int main(void)
                     }
                   
                 }
-                
+               
                 DrawGrid(50, 10.0f);         // Draw a grid
 
              
             EndMode3D();
 
+            //GUI
+            if(isGUIOpen)
+            {
+                
+                DrawLine(GetScreenWidth() -  GetScreenWidth()/4, 0, GetScreenWidth() -  GetScreenWidth()/4, GetScreenHeight(), Fade(LIGHTGRAY, 0.6f));
+                DrawRectangle(GetScreenWidth() -  GetScreenWidth()/4, 0, GetScreenWidth()/4, GetScreenHeight(), Fade(LIGHTGRAY, 0.3f));
+
+
+                //Set Color
+                object[selectedType][selectedId].color.r = (int)GuiSliderBar((Rectangle){ 1000, 90, 105, 20 }, "Red", NULL, object[selectedType][selectedId].color.r, 0, 255);
+                object[selectedType][selectedId].color.g = (int)GuiSliderBar((Rectangle){ 1000, 120, 105, 20 }, "Green", NULL, object[selectedType][selectedId].color.g, 0, 255);
+                object[selectedType][selectedId].color.b = (int)GuiSliderBar((Rectangle){ 1000, 150, 105, 20 }, "Blue",NULL, object[selectedType][selectedId].color.b, 0, 255);
+
+            }
+            
+            
             // if (currentlySelecting) 
             // {
             //     DrawText("1", GetScreenWidth() - 110, 10, 30, GREEN);
